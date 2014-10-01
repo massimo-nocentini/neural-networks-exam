9740751b2d7b2f8468373b91d8ca9c65ec85356f
Massimo Nocentini <massimo.nocentini@gmail.com>
2014-10-01 14:01:49 +0200

    Fix a little error that doesn't consider the first line, taking it as an header line instead.

--- R-attempts/inputs-normalization.R
+++ R-attempts/inputs-normalization.R
@@ -1,4 +1,4 @@
 norm.fun <- function(x){(x - min(x))/(max(x) - min(x))}
-inputs.matrix <- read.csv("inputs.txt", sep=" ")
+inputs.matrix <- read.csv("inputs.txt", sep=" ", header=FALSE)
 inputs.normalized <- apply (inputs.matrix, 2, norm.fun)
 write.table (inputs.normalized, "inputs-normalized.txt")
